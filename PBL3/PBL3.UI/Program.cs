57634924c57c0101a3dfe8f3f7f9f742a5e5baa5
imisseva <nguyenphong12a1nh3@gmail.com>
2025-05-14 18:31:31 +0700

    123

--- PBL3/PBL3.UI/Program.cs
+++ PBL3/PBL3.UI/Program.cs
@@ -16,7 +16,7 @@ namespace PBL3.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm2());
+            Application.Run(new StaffForm2());
         }
     }
 }
---
+++ PBL3/PBL3.UI/StaffForm2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL3.UI
+{
+    public partial class StaffForm2: Form
+    {
+        public StaffForm2()
+        {
+            InitializeComponent();
+        }
+
+        bool bookTicketExpand = false;
+        private void ticketbookingTransition_Tick(object sender, EventArgs e)
+        {
+            if (bookTicketExpand == false)
+            {
+                ticketbookingContainer.Height += 10;
+                if (ticketbookingContainer.Height >= 243)
+                {
+                    ticketbookingTransition.Stop();
+                    bookTicketExpand = true;
+                }
+            }
+            else
+            {
+                ticketbookingContainer.Height -= 15;
+                if (ticketbookingContainer.Height <= 48)
+                {
+                    ticketbookingTransition.Stop();
+                    bookTicketExpand= false;
+                }
+            }
+
+        }
+
+        private void btTicket_Click(object sender, EventArgs e)
+        {
+            ticketbookingTransition.Start();
+        }
+
+
+      
+    }
+}
