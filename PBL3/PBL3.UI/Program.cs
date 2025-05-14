c0f03dc06a332d8a025bb912cfdc97f31b1ba537
imisseva <nguyenphong12a1nh3@gmail.com>
2025-05-14 18:32:28 +0700

    1235

--- PBL3/PBL3.UI/Program.cs
+++ PBL3/PBL3.UI/Program.cs
@@ -16,7 +16,7 @@ namespace PBL3.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new StaffForm2());
+            Application.Run(new LoginForm2());
         }
     }
 }
