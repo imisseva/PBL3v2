6acddc3b0b28b443508055ef73601d5dc2b50ce9
ttoannp <thaitoan19825@gmail.com>
2025-05-19 01:38:20 +0700

    m

--- PBL3/PBL3.DTO/BusTicketDTO.cs
+++ PBL3/PBL3.DTO/BusTicketDTO.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
 
 namespace PBL3.DTO
 {
     public class BusTicketDTO
     {
+        public string ID_schedule {  get; set; }
         public string BusID { get; set; }
         public string SeatType { get; set; }
         public int Quantity { get; set; }
