e55e250a9e37525e8d7b8705bdf40c4c0d8dda66
imisseva <nguyenphong12a1nh3@gmail.com>
2025-06-09 03:29:52 +0700

    1

--- PBL3/PBL3.DTO/StaffDTO.cs
+++ PBL3/PBL3.DTO/StaffDTO.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-namespace PBL3.DTO
-{
-    public class StaffDTO
+    namespace PBL3.DTO
     {
-        public int ID_account { get; set; }
-        public string Name { get; set; }
-        public string email { get; set; }
-        public string phone { get; set; }
-        public string CCCD { get; set; }
-        public string home_address { get; set; }
-        public DateTime Dob { get; set; }
-        public string NoiSinh { get; set; }
-        public string Gender { get; set; }
-        public string ID_station { get; set; }   
-        
+        public class StaffDTO
+        {
+            public int ID_account { get; set; }
+            public string Name { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; }
+            public string CCCD { get; set; }
+            public string home_address { get; set; }
+            public DateTime Dob { get; set; }
+            public string NoiSinh { get; set; }
+            public string Gender { get; set; }
+            public string ID_station { get; set; }
+            public byte[] AvatarImage { get; set; }
     }
 }
