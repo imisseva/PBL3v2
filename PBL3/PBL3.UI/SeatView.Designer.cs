11490d1750ed1d68a7f932dc074df10ee5e7a02a
imisseva <nguyenphong12a1nh3@gmail.com>
2025-06-08 21:01:47 +0700

    123

--- PBL3/PBL3.UI/SeatView.Designer.cs
+++ PBL3/PBL3.UI/SeatView.Designer.cs
@@ -28,54 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnDel = new System.Windows.Forms.Button();
-            this.btnEdit = new System.Windows.Forms.Button();
-            this.btnAdd = new System.Windows.Forms.Button();
-            this.btnSearch = new System.Windows.Forms.Button();
             this.cbbPickBus = new Guna.UI2.WinForms.Guna2ComboBox();
             this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnSearch = new Guna.UI2.WinForms.Guna2Button();
+            this.btnAdd = new Guna.UI2.WinForms.Guna2Button();
+            this.btnEdit = new Guna.UI2.WinForms.Guna2Button();
+            this.btnDel = new Guna.UI2.WinForms.Guna2Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnDel
-            // 
-            this.btnDel.Location = new System.Drawing.Point(697, 25);
-            this.btnDel.Name = "btnDel";
-            this.btnDel.Size = new System.Drawing.Size(99, 23);
-            this.btnDel.TabIndex = 4;
-            this.btnDel.Text = "Xóa";
-            this.btnDel.UseVisualStyleBackColor = true;
-            this.btnDel.Click += new System.EventHandler(this.btnDelete_Click);
-            // 
-            // btnEdit
-            // 
-            this.btnEdit.Location = new System.Drawing.Point(574, 25);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(99, 23);
-            this.btnEdit.TabIndex = 5;
-            this.btnEdit.Text = "Sửa";
-            this.btnEdit.UseVisualStyleBackColor = true;
-            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
-            // 
-            // btnAdd
-            // 
-            this.btnAdd.Location = new System.Drawing.Point(449, 25);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(103, 23);
-            this.btnAdd.TabIndex = 6;
-            this.btnAdd.Text = "Thêm";
-            this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
-            // 
-            // btnSearch
-            // 
-            this.btnSearch.Location = new System.Drawing.Point(326, 25);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(105, 23);
-            this.btnSearch.TabIndex = 7;
-            this.btnSearch.Text = "Tìm kiếm";
-            this.btnSearch.UseVisualStyleBackColor = true;
-            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // cbbPickBus
             // 
@@ -90,33 +50,100 @@
             this.cbbPickBus.HoverState.Parent = this.cbbPickBus;
             this.cbbPickBus.ItemHeight = 30;
             this.cbbPickBus.ItemsAppearance.Parent = this.cbbPickBus;
-            this.cbbPickBus.Location = new System.Drawing.Point(33, 12);
+            this.cbbPickBus.Location = new System.Drawing.Point(33, 25);
             this.cbbPickBus.Name = "cbbPickBus";
             this.cbbPickBus.ShadowDecoration.Parent = this.cbbPickBus;
-            this.cbbPickBus.Size = new System.Drawing.Size(184, 36);
+            this.cbbPickBus.Size = new System.Drawing.Size(248, 36);
             this.cbbPickBus.TabIndex = 9;
             // 
             // dgv
             // 
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(33, 119);
+            this.dgv.Location = new System.Drawing.Point(33, 87);
             this.dgv.Name = "dgv";
             this.dgv.RowHeadersWidth = 51;
             this.dgv.RowTemplate.Height = 24;
             this.dgv.Size = new System.Drawing.Size(763, 461);
             this.dgv.TabIndex = 11;
             // 
+            // btnSearch
+            // 
+            this.btnSearch.BorderRadius = 8;
+            this.btnSearch.CheckedState.Parent = this.btnSearch;
+            this.btnSearch.CustomImages.Parent = this.btnSearch;
+            this.btnSearch.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.HoverState.Parent = this.btnSearch;
+            this.btnSearch.Location = new System.Drawing.Point(317, 25);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.ShadowDecoration.Parent = this.btnSearch;
+            this.btnSearch.Size = new System.Drawing.Size(105, 36);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BorderRadius = 8;
+            this.btnAdd.CheckedState.Parent = this.btnAdd;
+            this.btnAdd.CustomImages.Parent = this.btnAdd;
+            this.btnAdd.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.HoverState.Parent = this.btnAdd;
+            this.btnAdd.Location = new System.Drawing.Point(442, 25);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.ShadowDecoration.Parent = this.btnAdd;
+            this.btnAdd.Size = new System.Drawing.Size(105, 36);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BorderRadius = 8;
+            this.btnEdit.CheckedState.Parent = this.btnEdit;
+            this.btnEdit.CustomImages.Parent = this.btnEdit;
+            this.btnEdit.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnEdit.ForeColor = System.Drawing.Color.White;
+            this.btnEdit.HoverState.Parent = this.btnEdit;
+            this.btnEdit.Location = new System.Drawing.Point(566, 25);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.ShadowDecoration.Parent = this.btnEdit;
+            this.btnEdit.Size = new System.Drawing.Size(105, 36);
+            this.btnEdit.TabIndex = 14;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDel
+            // 
+            this.btnDel.BorderRadius = 8;
+            this.btnDel.CheckedState.Parent = this.btnDel;
+            this.btnDel.CustomImages.Parent = this.btnDel;
+            this.btnDel.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnDel.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnDel.ForeColor = System.Drawing.Color.White;
+            this.btnDel.HoverState.Parent = this.btnDel;
+            this.btnDel.Location = new System.Drawing.Point(691, 25);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.ShadowDecoration.Parent = this.btnDel;
+            this.btnDel.Size = new System.Drawing.Size(105, 36);
+            this.btnDel.TabIndex = 15;
+            this.btnDel.Text = "Xoá";
+            // 
             // SeatView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(850, 608);
-            this.Controls.Add(this.dgv);
-            this.Controls.Add(this.cbbPickBus);
             this.Controls.Add(this.btnDel);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.cbbPickBus);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "SeatView";
             this.Text = "SeatView";
@@ -126,11 +153,11 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button btnDel;
-        private System.Windows.Forms.Button btnEdit;
-        private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.Button btnSearch;
         private Guna.UI2.WinForms.Guna2ComboBox cbbPickBus;
         private System.Windows.Forms.DataGridView dgv;
+        private Guna.UI2.WinForms.Guna2Button btnSearch;
+        private Guna.UI2.WinForms.Guna2Button btnAdd;
+        private Guna.UI2.WinForms.Guna2Button btnEdit;
+        private Guna.UI2.WinForms.Guna2Button btnDel;
     }
 }
